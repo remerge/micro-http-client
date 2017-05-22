1f0c28d17e7c5ac021bd7e8b5ecd41831243d388
Will Madden <w.a.madden@gmail.com>
2017-05-22 12:16:07 +0200

    (refactor) Make sure constructed client is named correctly

--- src/HttpClient.js
+++ src/HttpClient.js
@@ -2,14 +2,20 @@ async function processInterceptor(memo, interceptor) {
   return interceptor(await memo);
 }
 
-export default function HttpClient(options = {}) {
+function HttpClient(options = {}) {
   const {requestInterceptors = [], responseInterceptors = []} = options;
-  const client = {
+
+  const client = Object.create(HttpClient.prototype);
+
+  Object.assign(client, {
     async fetch(url, options) {
       let requestObject = await requestInterceptors.reduce(processInterceptor, Object.assign({}, {url}, options));
       let response = await fetch(requestObject.url, requestObject);
       return responseInterceptors.reduce(processInterceptor, response);
     },
-  };
+  });
+
   return client;
 }
+
+export default HttpClient;
